2a637650c3d1f915716d1b8568708d9ec9cdcd66
core.pager=cat - <mikepool599@gmail.com>
2021-10-20 14:52:38 +0100

    fixing + -

--- shop.js
+++ shop.js
@@ -3,6 +3,8 @@ var addToCartBtn = document.getElementsByClassName('btn-primary')
 var cartContainer = document.getElementsByTagName('table')[0]
 var itemQuantity = document.getElementsByClassName('num')
 var deleteBtn = document.getElementsByClassName('btn-danger')
+var addBtn = document.getElementsByClassName("btn-warning")
+var minusBtn = document.getElementsByClassName("btn-success")
 
 
 
@@ -24,12 +26,15 @@ function addToCart(event){
     let itemName = btnParent.children[1].innerText
     let itemPrice =btnParent.children[2].innerText
 
+
     itemContainer.innerHTML = ` 
     
     <td><img src=${itemImage} class="cartImg" alt="T's" height="100"></td>
     <td><h3 class="Item-name">${itemName}</h3></td>
     <td><h4 class="Item-Price">${itemPrice}</h4></td>
-    <td><input type = 'number' class = 'num' value = '1'></td>  
+    <td><button id="increment-btn" class="btn-warning" onclick="increment('count-${itemName}')">+</td>
+    <td><input type="number" value="1" id="count-${itemName}" class="num" style= "width: 50px;"></td>
+    <td><button id="decrement-btn" class="btn-success" onclick="decrement('count-${itemName}')">-</td>  
     <td><h4 class="Total-Price">${itemPrice}</h4></td>
     <td><button class="btn btn-danger" type= "buttom">Remove Item</button></td> 
 
@@ -47,6 +52,54 @@ function addToCart(event){
     }
 
     totalCartPrice()
+}
+
+function increment(counterId) {
+    let counterField = document.getElementById(counterId)
+    let count = +counterField.value
+    count += 1
+    console.log(count);
+    counterField.value = count 
+
+        let quantity = counterField
+        quantityParent = quantity.parentElement.parentElement
+        price = quantityParent.getElementsByClassName('Item-Price')[0]
+        totalPrice = quantityParent.getElementsByClassName('Total-Price')[0]
+        priceContent = price.innerText.replace('$', '') 
+        totalPrice.innerText = '$' + quantity.value * priceContent
+        totalCartPrice()
+        if(isNaN(quantity.value)|| quantity.value <= 0){
+            quantity.value = 1
+            totalPrice.innerText = '$' + quantity.value * priceContent
+            totalCartPrice()
+        }
+
+
+
+}
+
+function decrement(counterId) {
+    let counterField = document.getElementById(counterId)
+    let count = +counterField.value
+    if (count > 0) {
+        count -= 1
+        console.log(count);
+        counterField.value = count
+
+        let quantity = counterField
+        quantityParent = quantity.parentElement.parentElement
+        price = quantityParent.getElementsByClassName('Item-Price')[0]
+        totalPrice = quantityParent.getElementsByClassName('Total-Price')[0]
+        priceContent = price.innerText.replace('$', '') 
+        totalPrice.innerText = '$' + quantity.value * priceContent
+        totalCartPrice()
+        if(isNaN(quantity.value)|| quantity.value <= 0){
+            quantity.value = 1
+            totalPrice.innerText = '$' + quantity.value * priceContent
+            totalCartPrice()
+        }
+        
+    }      
 }
 
 
@@ -86,20 +139,6 @@ function deleteCart(event){
         totalCartPrice()
     
 } 
-
-/*
-function colorIt(id) {
-    //var likeBtn = document.getElementById(id).setAttribute("class", "fas fa-heart");
-    var likeBtn = document.getElementById(id)
-    if(likeBtn.classList.contains("far")){
-        likeBtn.classList.remove("far");
-        likeBtn.classList.add("fas");
-    }else{
-        likeBtn.classList.remove("fas");
-        likeBtn.classList.add("far");
-    }
-}*/
-
 
 
 function colorIt(id) {
